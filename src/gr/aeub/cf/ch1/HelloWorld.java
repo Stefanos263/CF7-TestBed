9518da89da4f15c2e80a2834add6b51079ed96a0
Stefanos263 <stefanoskempas26@gmail.com>
2024-10-22 18:01:52 +0300

    Initial commit

---
+++ src/gr/aeub/cf/ch1/HelloWorld.java
@@ -0,0 +1,11 @@
+package gr.aeub.cf.ch1;
+
+/**Prints Hello Coding Factory in standard output (console).
+*/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Coding Factory");  
+    }
+
+}
